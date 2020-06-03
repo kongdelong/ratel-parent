39364e62f9f4d094d52e8ae76b471d2d75b42d92
dongchuang <yy6yy@live.cn>
2020-06-03 18:22:29 +0800

    更新

--- ratel-boot/src/main/java/com/ratel/modules/security/config/RetelBootSecurityConfig.java
+++ ratel-boot/src/main/java/com/ratel/modules/security/config/RetelBootSecurityConfig.java
@@ -2,15 +2,25 @@ package com.ratel.modules.security.config;
 
 
 import com.ratel.framework.modules.security.config.RetelSecurityConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class RetelBootSecurityConfig extends RetelSecurityConfig {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // 密码加密方式
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
